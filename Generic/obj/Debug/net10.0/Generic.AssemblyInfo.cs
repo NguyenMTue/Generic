f1fd8e6d7146eedb2c0618ae895ee727dfbf24df
NguyenMTue <23115053122342@sv.ute.udn.vn>
2026-03-18 00:44:53 +0700

    add Bai10

--- Generic/obj/Debug/net10.0/Generic.AssemblyInfo.cs
+++ Generic/obj/Debug/net10.0/Generic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Generic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9c29d3aea085e2f604b7c61dfaa6077a6034fa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d2859cf91ddfb701c304d7ab902f31dac16ac4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Generic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Generic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
