3ad50c031e98b5b2dfe66de45d5b6d228a757f20
NguyenMTue <23115053122342@sv.ute.udn.vn>
2026-03-18 00:52:07 +0700

    add Bai13

--- Generic/obj/Debug/net10.0/Generic.AssemblyInfo.cs
+++ Generic/obj/Debug/net10.0/Generic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Generic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd383316d8ea159bbdd25a1013e6c95acd6668f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e386a42c3e4f133efe938fbfc56b2ac2327a98d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Generic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Generic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
